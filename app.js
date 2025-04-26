a0592b63976403a62b4b2f7d385dda45ee5f7784
cuberquest <134105195+lascyrx@users.noreply.github.com>
2025-04-26 02:07:57 -0400

    v0.0.2 - adding canvas auto-resize function

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ let hs = document.getElementById("hs"), highscore = localStorage.hs / 1 || 0;
 localStorage.hs = highscore;
 hs.innerHTML = highscore;
 
+function resize() {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+}
+addEventListener("resize", resize);
+
 const game = new Game(canvas);
 game.render = game.start();
